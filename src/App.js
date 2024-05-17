08fee5c73188e8b941b743515ec10cfc42325503
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2024-05-17 21:03:20 +0300

    Update app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { ChatEngine } from "react-chat-engine";
 import "./App.css";
 import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
 
 const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
   return (
     <ChatEngine
       height="100vh"
       projectID="9da8a72f-6d71-47f8-a4ff-29e116e3983a"
-      userName="johnbuh"
-      userSecret="1234321"
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
